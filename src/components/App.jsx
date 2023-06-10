97812b30fd5e55928a07c717a079a0d142ea156a
Oleksandr Rakomin <uamilliardua@gmail.com>
2023-06-10 14:01:11 +0200

    add stap 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,58 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import { nanoid } from 'nanoid';
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: '',
+  };
+
+  onChange = e => {
+    const { name, value } = e.target;
+
+    this.setState({ [name]: value });
+  };
+
+  onAddButtonClick = e => {
+    e.preventDefault();
+    const id = nanoid(16);
+
+    this.setState(prevState => {
+      const { name, number } = this.state;
+
+      return {
+        contacts: [...prevState.contacts, { name, number, id }],
+      };
+    });
+
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    const {
+      state,
+      state: { contacts },
+      onChange,
+      onAddButtonClick,
+    } = this;
+
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm
+          value={state}
+          onChange={onChange}
+          onAddButtonClick={onAddButtonClick}
+        />
+
+        <h2>Contacts</h2>
+        {/* <Filter ... /> */}
+        <ContactList contacts={contacts} />
+      </div>
+    );
+  }
+}
+
+export default App;
